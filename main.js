32b8c9cc658b0f69970a54466d2efec5af1e2175
judy97678 <judy97678@gmail.com>
2022-05-10 11:58:32 +0800

    init commit

---
+++ main.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    $(function(){
+        if($(window).width()<944){ //當視窗小於944時才運作
+        $('.navbar a').on('click', function(){
+            $('.menu-btn').click();
+          });
+        }
+        });
+
+        // slide-up script
+        $('.scroll-up-btn').click(function(){
+            $('html').animate({scrollTop: 0});
+            // removing smooth scroll on slide-up button click
+            $('html').css("scrollBehavior", "auto");
+        });
+        $('.navbar .menu li a').click(function(){
+            // applying again smooth scroll on menu items click
+            $('html').css("scrollBehavior", "smooth");
+        });
+        // toggle menu/navbar script
+        $('.menu-btn').click(function(){
+            $('.navbar .menu').toggleClass("active");
+            $('.menu-btn i').toggleClass("active");
+        });
+        
+     // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["小宮燈", "青少年組", "大宮燈", "不分年齡"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+})
